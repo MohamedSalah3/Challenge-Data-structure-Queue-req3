384ad7bb697dab6e963900daa3573e2a4b4cf28c
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-02-06 20:04:16 +0200

    Working but under testing

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "Queue.h"
+int main() {
+uint8_t size_needed=0;
+uint8_t data_return=0;
+printf("Enter the maximum num of elements \n");
+scanf("%d",&size_needed);
+Queue Data_base2;
+G_Queue_ptr=&Data_base2;
+createQueue(G_Queue_ptr,size_needed);
+enqueue(G_Queue_ptr,10);
+//printf("%d\n",G_Queue_ptr->ptr_to_queue_arr[0]);
+dequeue(G_Queue_ptr,&data_return);
+//printf("%d\n",G_Queuea_ptr->ptr_to_queue_arr[0]);
+
+  return 0;
+}
